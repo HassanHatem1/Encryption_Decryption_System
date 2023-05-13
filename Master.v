8081dd9d3375a16c01a7aaa09df71039468b02c8
salah <salah.m.abotaleb@gmail.com>
2023-05-13 14:01:35 +0300

    cleaning repo

--- Master.v
+++ Master.v
@@ -2,18 +2,21 @@ module Master #(
     parameter nk=8,parameter nb=4,parameter nr=14
 )(
 input  Miso ,		//1-bit input from  slave  
-//input in_real_key ,		//1-bit input from  slave  
-input Out_clk ,		//1-bit input from clock from  slave  
+//input in_real_key ,		//1-bit input from  slave    
 input [8*4*nb-1:0]from_Real_msg,		
-input [(32*nb*(nr+1))-1:0]from_Real_key, 
-//input [(32*nb*(nr+1))-1:0]from_enc_dec_key,        //128-bit input from slave(Enc/dyc)
+input [(32*nk)-1:0]from_Real_key,
+input data_done, 
+//input [((32*nk))-1:0]from_enc_dec_key,        //128-bit input from slave(Enc/dyc)
 //
 input in_clk, //needs to be generated
+input rst,
 //
+
+output out_clk ,		//1-bit input from clock from  slave
 output reg cs_enc_dec ,           //1-bit output to  slave  
 output reg Mosi ,		//1-bit output to  slave  
 //output reg out_real_key ,		//1-bit output to  slave  
-output reg [8*4*nb-1:0]Out_real_msg		//generic bit output to slave(Enc/dyc)
+output reg [8*4*nb-1:0]Sipo_Register		//generic bit output to slave(Enc/dyc)
        //generic bit output to slave(Enc/dyc)
 
 
@@ -21,48 +24,65 @@ output reg [8*4*nb-1:0]Out_real_msg		//generic bit output to slave(Enc/dyc)
 //------------Code Starts Here-------------------------
 
 integer countmsg;
-integer countmsgout_key;
-integer countencdec;
-reg [8*4*nb-1:0]Sipo_Register;
-reg [8*4*nb+32*nb*(nr+1)-1:0]Piso_Register;
+integer countmsgout;
+//integer countencdec;
+//reg [8*4*nb-1:0]Sipo_Register;
+reg [8*4*nb+(32*nk)-1:0]Piso_Register;
 reg dataready;
 ////////
-always @(from_Real_msg , from_Real_key )
+assign out_clk=in_clk;
+always @(from_Real_msg , from_Real_key)
 begin
+Piso_Register<={from_Real_msg,from_Real_key};
 Sipo_Register<=0;
-Piso_Register<=0;
 countmsg<=0;
-countmsgout<=8*4*nb+32*nb*(nr+1)-1;//might need change 
+countmsgout<=8*4*nb+(32*nk)-1;//might need change 
 dataready<=0;
-countencdec<=0;
+cs_enc_dec<=0;
+//countencdec<=0;
 end
 ////////
-
-///////
-always @(posedge in_clk )
+always @(negedge data_done)
 begin
-if(countmsg<=8*4*nb-1)
+    cs_enc_dec<=1;
+end
+always @(negedge rst)
+begin 
+    cs_enc_dec<=0;
+end
+///////
+always @(posedge in_clk, posedge rst)
+begin
+if(rst)
+begin
+Sipo_Register<=0;
+countmsg<=0;
+countmsgout<=8*4*nb+(32*nk)-1;//might need change 
+dataready<=0;
+cs_enc_dec<=1;
+end
+else 
+if(countmsg<=8*4*nb-1 && data_done==1)
 begin
 countmsg<=countmsg+1;
-Sipo_register<={Sipo_register[8*4*nb-2:0], Miso};
+Sipo_Register<={Sipo_Register[8*4*nb-2:0], Miso};
 end
-else if(countmsg==8*4*nb)
+/*else if(countmsg==8*4*nb)
 begin
 countencdec<=countencdec+1;
 end
 if(countencdec==20)
 begin
 Out_real_msg<=Sipo_Register;
-end
+end*/
 end
 //////
 
-assign Piso_Register={from_Real_msg,from_Real_key};
 
 ////
 always @(posedge in_clk)
 begin
-if(countmsgout >=0 && dataready)
+if(countmsgout >=0)
 begin
 Mosi<=Piso_Register[countmsgout];
 countmsgout<=countmsgout-1;
@@ -70,6 +90,7 @@ end
 if (countmsgout==-1)
 begin
 dataready<=0;
+Mosi<=1'bz;
 end
 end
 ////
--- Subnode.v
+++ Subnode.v
@@ -5,14 +5,14 @@ input sdi ,		//1-bit input from Master World
 //input in_real_key ,		//1-bit input from Master World
 input in_clk ,		//1-bit input from clock from Master World
 input [8*4*nb-1:0]from_enc_dec_msg,		//128-bit input from slave(Enc/dyc)
-//input [(32*nb*(nr+1))-1:0]from_enc_dec_key,        //128-bit input from slave(Enc/dyc)
-input cs_real_world ,           //1-bit input from Master World
-
+//input [((32*nk))-1:0]from_enc_dec_key,        //128-bit input from slave(Enc/dyc)
+input cs ,           //1-bit input from Master World
+input rst,
 output reg sdo ,		//1-bit output to Master World
 //output reg out_real_key ,		//1-bit output to Master World
 output reg [8*4*nb-1:0]to_enc_dec_msg,		//generic bit output to slave(Enc/dyc)
-output reg [(32*nb*(nr+1))-1:0]to_enc_dec_key,        //generic bit output to slave(Enc/dyc)
-output reg cs_slave             //1-bit output to slaves
+output reg [((32*nk))-1:0]to_enc_dec_key,        //generic bit output to slave(Enc/dyc)
+output reg data_done             //1-bit output to slaves
 
 ); // End of port list
 //------------Code Starts Here-------------------------
@@ -21,12 +21,16 @@ integer countkey;
 integer countmsgout;
 integer countencdec;
 reg [8*4*nb-1:0]Sipo_Register;
-reg [(32*nb*(nr+1))-1:0]Key_Register;
+reg [((32*nk))-1:0]Key_Register;
 reg [8*4*nb-1:0]Piso_Register;
 reg dataready;
-always @(negedge in_clk )
+reg snow;
+reg once;
+always @(rst)
 begin
-if(cs)
+    once=1;
+end
+always @(negedge cs)
 begin
 Sipo_Register<=0;
 Key_Register<=0;
@@ -34,22 +38,28 @@ Piso_Register<=0;
 countmsg<=0;
 countkey<=0;
 countmsgout<=8*4*nb-1;
+data_done<=0;
 dataready<=0;
 countencdec<=0;
+snow<=1;
 end
-else
+
+always @(negedge in_clk )
+begin
+
+if(!cs && (once || !snow))
 begin
 if(countmsg<=8*4*nb-1)
 begin
 countmsg<=countmsg+1;
-Sipo_register<={Sipo_register[8*4*nb-2:0],sdi};
+Sipo_Register={Sipo_Register[8*4*nb-2:0],sdi};
 end
-else if(countmsg==8*4*nb && countkey<=32*nb*(nr+1)-1)
+else if(countmsg==8*4*nb && countkey<=(32*nk)-1)
 begin
-Key_Register<={Key_Register[(32*nb*(nr+1))-2:0],in_real_key};
+Key_Register={Key_Register[((32*nk))-2:0],sdi};
 countkey<=countkey+1;
 end
-else if(countmsg==8*4*nb && countkey==32*nb*(nr+1)-1)
+else if(countmsg==8*4*nb && countkey==(32*nk) && !dataready)
 begin
 countencdec<=countencdec+1;
 end
@@ -57,13 +67,19 @@ if(countencdec==20)
 begin
 Piso_Register<=from_enc_dec_msg;
 dataready<=1;
+countencdec<=0;
 end
+end
+else
+begin
+    snow<=0;
+    once<=0;
 end
 end
 
 always @(negedge in_clk)
 begin
-if(countmsg ==8*4*nb && countkey==32*nb*(nr+1) &&!cs)
+if(countmsg ==8*4*nb && countkey==(32*nk) &&!cs)
 begin
 to_enc_dec_msg<=Sipo_Register;
 to_enc_dec_key<=Key_Register;
@@ -74,12 +90,15 @@ always @(negedge in_clk)
 begin
 if(countmsgout >=0 && dataready && !cs)
 begin
+data_done<=1;
 sdo<=Piso_Register[countmsgout];
 countmsgout<=countmsgout-1;
 end
 if (countmsgout==-1 && !cs)
 begin
+data_done<=0;
 dataready<=0;
+sdo=0;
 end
 end
 
